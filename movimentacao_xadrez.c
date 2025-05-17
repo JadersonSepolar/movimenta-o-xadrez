cec09055ce23dc33ee21bf754fc37f965919cb80
JadersonSepolar <jsepolar@gmail.com>
2025-05-17 22:04:15 +0000

    movimento do cavalo

--- movimentacao_xadrez.c
+++ movimentacao_xadrez.c
@@ -5,11 +5,13 @@ int main(){
 int i = 1;
 
 
+
 while (i <= 5){
      printf("Direita.\n");//torre: 5 casas para direita
-     i++;
-}
- i = 1;
+         i++;
+    }
+         i = 1;
+
 do {
     printf("Cima,Direita.\n");//Bispo: 5 casas direita diagonal para cima
     i++;
@@ -17,9 +19,21 @@ do {
 
 
 for (i = 0; i < 8; i++)
-{
+    {
     printf("Esquerda.\n");// Rainha: 8 casas a esquerda
-}
+    }
+    printf("--------------------------------------\n");
+
+   
+for (i = 1; i <= 2; i++)
+    {
+        
+        for(int j = 1; j <= 1; j++)
+        {
+            printf("Baixo.\n");
+        }      
+    }
+        printf("Esquerda.\n");
 
     return 0;
 }
